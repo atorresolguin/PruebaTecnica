182e570b85801311070a88eed79890a47c5feefb
Arturo Torres <arturo.to.ol@gmail.com>
2022-08-24 11:58:59 -0400

    Fix menor

--- src/testbackend/MainView.java
+++ src/testbackend/MainView.java
@@ -5,7 +5,7 @@ public class MainView {
 
 	public static void main(String[] args) {
 		System.out.println(ServicioInvertirFrase.invertir("Esta frase debe darse vuelta"));
-		System.out.println(ServicioOrdenarNumeros.ordenar("5678319241560"));
+		System.out.println(ServicioOrdenarNumeros.ordenar("56783192415"));
 		ServicioPersonas.menuPersonas();
 	}
 
